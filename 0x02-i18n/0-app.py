9c399eb15145f20df409b7833a8ddf75e6d9c008
Mohanned-sameh <mohanned2678@gmail.com>
2024-05-29 00:19:01 +0300

    chore: Refactor home route to improve code readability and maintainability

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python3
 from flask import Flask, render_template
 
+""" Basic flask setup """
 app = Flask(__name__)
+
+""" home route """
 
 
 @app.route("/")
 def hello_world():
+    """return the home template"""
     return render_template("0-index.html")
 
 
 if __name__ == "__main__":
+    """ Main Function"""
     app.run(host="127.0.0.1", port=5000)
